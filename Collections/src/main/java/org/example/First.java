aa0461e31ded8a513f4f2191c7f7d1f9d957b86b
charan1409 <charankallempudi24@gmail.com>
2024-05-10 17:28:53 +0530

    folders created

--- Collections/src/main/java/org/example/First.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class First {
-    public static void main(String[] args) {
-        int k=89;
-        Integer i1=k;
-        System.out.println(i1);
-    }
-}
--- Naming_Convention/src/main/java/org/example/Player.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Player {
-    public Player(String name,int age,int runs) {
-        System.out.println("name"+name+"  "+"age"+age+"   "+"runs"+runs);
-    }
-}
