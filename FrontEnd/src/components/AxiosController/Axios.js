e6c7ec3f5b678365135caca32e996f3cc752282f
charan1409 <charankallempudi24@gmail.com>
2024-05-30 11:26:54 +0530

    changed axios config

--- FrontEnd/src/components/AxiosController/Axios.js
+++ FrontEnd/src/components/AxiosController/Axios.js
@@ -2,8 +2,5 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL: "http://40.76.111.161:8080",
-  headers: {
-    "Content-type": "application/json",
-  },
 });
 export default Axios;
